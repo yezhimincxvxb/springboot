9b2cf77bac7fe6cc08a2268480b62d8b33e704f9
yezhimincxvxb <395995486@qq.com>
2021-08-07 15:07:01 +0800

    springboot

--- thymeleaf/src/main/java/com/yzm/thymeleaf/controller/HelloController.java
+++ thymeleaf/src/main/java/com/yzm/thymeleaf/controller/HelloController.java
@@ -2,64 +2,54 @@ package com.yzm.thymeleaf.controller;
 
 import com.yzm.thymeleaf.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/ha")
     public String ha() {
-        return "redirect:hello.html";
+        return "redirect:index.html";
     }
 
     @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView mav) {
+    public ModelAndView hello(ModelAndView mav, HttpServletRequest request) {
         mav.setViewName("/hello");
         mav.addObject("title", "欢迎使用Thymeleaf！！！");
-        return mav;
-    }
 
-    @GetMapping("/format")
-    public String format(ModelMap map) {
-        map.addAttribute("date", new Date());
-        map.addAttribute("calendar", Calendar.getInstance());
-        return "/hello";
-    }
-
-    @GetMapping("/hello2")
-    public String hello2(ModelMap map, HttpServletRequest request) {
         User user = new User();
         user.setUsername("Yzm");
         request.getSession().setAttribute("user", user);
-        map.addAttribute("welcomeMsgKey", "home.welcome2");
-        return "/hello2";
+        mav.addObject("welcomeKey", "hello.welcome");
+
+        mav.addObject("date", new Date());
+        mav.addObject("calendar", Calendar.getInstance());
+        return mav;
     }
 
-    @GetMapping("/hello3")
-    public String hello3(ModelMap map) {
+    @GetMapping("/hello2")
+    public String hello2(Map<String, Object> map) {
         User user = new User();
         user.setUserId(1);
         user.setUsername("AAA");
         user.setPassword("aaa");
-        map.addAttribute("user", user);
-        return "/hello3";
+        map.put("user", user);
+        return "hello2";
     }
 
-    @GetMapping("/hello4")
-    public String hello4(ModelMap map, Integer id, String name) {
+    @GetMapping("/hello3")
+    public String hello3(Model map, Integer id, String name) {
         map.addAttribute("id", id);
         map.addAttribute("name", name);
-        return "/hello4";
+        return "hello3";
     }
 
     @GetMapping("/order/info")
@@ -68,8 +58,8 @@ public class HelloController {
         return id + "：" + name;
     }
 
-    @GetMapping("/users")
-    public String users(ModelMap map) {
+    @GetMapping("/hello4")
+    public String hello4(ModelMap map) {
         List<User> users = new ArrayList<>();
         User user = new User();
         user.setUserId(1);
@@ -88,8 +78,70 @@ public class HelloController {
         users.add(user);
         users.add(user2);
 
+        Map<String, User> map1 = new HashMap<>();
+        map1.put("user", user);
+        map1.put("user2", user2);
+
         map.addAttribute("users", users);
-        return "/user";
+        map.addAttribute("username", "yzm");
+        map.addAttribute("studentmap", map1);
+        return "hello4";
+    }
+
+    @GetMapping("/hello5")
+    public String hello6(ModelMap map) {
+        User user = new User();
+        user.setUsername("yzm");
+        map.addAttribute("title", "用户");
+        map.addAttribute("user", user);
+        map.addAttribute("name", "张三");
+        map.addAttribute("age", 33);
+        map.addAttribute("phone", 110);
+        return "hello5";
+    }
+
+    @GetMapping("/user/info")
+    public ModelAndView hello7(Model model) {
+        User user = new User();
+        user.setUserId(1);
+        user.setUsername("yzm");
+        user.setPassword("123");
+        model.addAttribute("user", user);
+        return new ModelAndView("/user/add", "userModel", model);
+    }
+
+    @PostMapping("/user/add")
+    public String add(User user) {
+        System.out.println("user = " + user);
+        return "/user/success";
+    }
+
+    @GetMapping("/frag")
+    public String frag(ModelMap map) {
+        map.addAttribute("id", 1);
+        map.addAttribute("name", "yzm");
+        map.addAttribute("age", 25);
+        return "/fragments/frag";
+    }
+
+    @GetMapping("/frag2")
+    public String frag2() {
+        return "/fragments/frag2";
+    }
+
+    @GetMapping("/frag3")
+    public String frag3() {
+        return "/fragments/frag3";
+    }
+
+    @GetMapping("/frag4")
+    public String frag4() {
+        return "/fragments/frag4";
+    }
+
+    @GetMapping("/frag5")
+    public String frag5() {
+        return "/fragments/frag5";
     }
 
 }
