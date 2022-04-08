61593bbd494c6350bb04bdf6040f07144988a3c1
yezhimincxvxb <395995486@qq.com>
2022-04-08 17:56:06 +0800

    springboot

---
+++ thread/src/main/java/com/yzm/thread/ThreadApplication.java
@@ -0,0 +1,15 @@
+package com.yzm.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.CyclicBarrier;
+
+@SpringBootApplication
+public class ThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadApplication.class, args);
+    }
+
+}
