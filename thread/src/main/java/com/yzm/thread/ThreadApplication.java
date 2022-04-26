c3c23290326f5fec09652b82122d86831f05c9dc
yezhimincxvxb <395995486@qq.com>
2022-04-26 17:27:45 +0800

    springboot

--- thread/src/main/java/com/yzm/thread/ThreadApplication.java
+++ thread/src/main/java/com/yzm/thread/ThreadApplication.java
@@ -2,10 +2,11 @@ package com.yzm.thread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import java.util.concurrent.CyclicBarrier;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ThreadApplication {
 
     public static void main(String[] args) {
